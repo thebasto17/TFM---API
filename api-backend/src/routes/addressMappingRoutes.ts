d0ae23013c9e0084fd4cfbcd18358874927b0508
Marc Català <mcatala@MacBook-Pro-de-Marc.local>
2024-10-19 19:58:50 +0200

    Api CG - retreive Monero Price

--- api-backend/src/routes/addressMappingRoutes.ts
+++ api-backend/src/routes/addressMappingRoutes.ts
@@ -6,11 +6,14 @@ import {
   deleteMappingHandler
 } from '../controllers/addressMappingController';
 
+import { getMoneroPrice } from '../controllers/coingeckoMappingController';
+
 const router = Router();
 
 router.post('/', createMappingHandler);
 router.get('/:moneroTxId/:moneroTxKey', getMappingHandler);
 router.put('/:moneroTxId/:moneroTxKey', updateMappingHandler);
 router.delete('/:moneroTxId/:moneroTxKey', deleteMappingHandler);
+router.get('/monero-price', getMoneroPrice);
 
 export default router;
