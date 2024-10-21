890ae48bb29d257e415abc5006e29ca64d3c6224
ikcoin <ikcoin.kb@protonmail.com>
2024-10-21 20:31:20 +0200

    fix: mappings retrieval and random calculation

--- api-backend/src/services/moneroService.ts
+++ api-backend/src/services/moneroService.ts
@@ -1,4 +1,7 @@
 import moneroTs from "monero-ts";
+import { deleteMapping, listMappings } from "../services/addressMappingService";
+import { mintTokens } from "../services/ethereumService";
+import { IAddressMapping } from "../models/addressMapping";
 
 export async function loadWallet() {
   return moneroTs.openWalletFull({
@@ -21,24 +24,13 @@ export async function monitorWallet(wallet: moneroTs.MoneroWalletFull) {
         let isLocked = output.getTx().getIsLocked();
 
         if (isConfirmed && isLocked) {
-          const mappings = [
-            {
-              address: "0x123123",
-              randomNumber: 9999799999,
-              assetPrice: 100000000000000,
-              amountToTransfer: 120000000000000,
-            },
-            {
-              address: "0x444444",
-              randomNumber: 0,
-              assetPrice: 10000000000000,
-              amountToTransfer: 10000000000000,
-            },
-          ]; //getAllMappings()
-          mappings.forEach((mapping) => {
+          const mappings = await listMappings();
+          mappings.forEach(async (mapping: IAddressMapping) => {
             if (isMatchingTransaction(Number(txAmount), mapping)) {
               // 1. Trigger Smart Contract minting
+              //await mintTokens(_, mapping.ethereumAddress);
               // 2. Delete mapping
+              await deleteMapping(mapping.randomNumber.toString());
             }
           });
         }
@@ -48,6 +40,6 @@ export async function monitorWallet(wallet: moneroTs.MoneroWalletFull) {
 }
 
 function isMatchingTransaction(txAmount: number, mapping: any) {
-  const randomNumberFromTx = Math.round((Number(txAmount) - mapping.assetPrice) * 10e11);
-  return randomNumberFromTx === mapping.randomNumber;
+  const randomNumberFromTx = Math.round((Number(txAmount) - Number(mapping.assetPrice)) * 10e11);
+  return randomNumberFromTx === Number(mapping.randomNumber);
 }
