b1490d16468e999d2b0acb0f5286c2d529a1e9a4
iMofli <jordiperez10@icloud.com>
2024-10-25 19:47:31 +0200

    Reorganize routes

--- api-backend/src/app.ts
+++ api-backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import addressMappingRoutes from './routes/addressMappingRoutes';
+import priceExchangeRoutes from "./routes/priceExchangeRoutes";
 
 dotenv.config();
 
@@ -10,5 +11,6 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use('/address-mapping', addressMappingRoutes);
+app.use('/prices', priceExchangeRoutes);
 
 export default app;
--- api-backend/src/services/ethereumService.ts
+++ api-backend/src/services/ethereumService.ts
@@ -2,7 +2,8 @@ import { ethers } from 'ethers';
 import ERC20ABI from "../abis/ERC20Token.json";
 
 const provider = new ethers.JsonRpcProvider('http://localhost:8545');
-const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
+const private_key = process.env.PRIVATE_KEY ?? '';
+const wallet = new ethers.Wallet(private_key, provider);
 
 export const mintTokens = async (contractAddress: string, toAddress: string) => {
     try {
