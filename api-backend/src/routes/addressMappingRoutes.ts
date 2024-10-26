5b6ff564254174544f6eaff73e71547f46d4f297
Marc Català <mcatala@MacBook-Pro-de-Marc.local>
2024-10-26 12:53:03 +0200

    feat: updated mapping routes

--- api-backend/src/routes/addressMappingRoutes.ts
+++ api-backend/src/routes/addressMappingRoutes.ts
@@ -6,15 +6,11 @@ import {
   deleteMappingHandler
 } from '../controllers/addressMappingController';
 
-import { getPrices, convertEthToXmr } from '../controllers/coingeckoMappingController';
-
 const router = Router();
 
 router.post('/', createMappingHandler);
 router.get('/:randomNumber', getMappingHandler);
 router.put('/:randomNumber', updateMappingHandler);
 router.delete('/:randomNumber', deleteMappingHandler);
-router.get('/price/prices', getPrices);
-router.get('/price/convertEthToXmr/:ethPrice/:xmrPrice/:price', convertEthToXmr);
 
 export default router;
